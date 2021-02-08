1b0f833a038a442255fc67177d5c3e2acc38720c
Tobias Breitkopf <tobias.breitkopf@googlemail.com>
2021-02-08 15:56:37 +0100

    Added Event System

---
+++ Assets/Scrips/EventSysetem/GameMaster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster gm;
+    private void Awake()
+    {
+        if(gm == null)
+        {
+            gm = this;
+        }
+        else
+        {
+            Debug.LogError("GameMaster allready exist, Destroying this !");
+            Destroy(gm);
+        }
+    }
+
+    public event Action<int> event_PlayerChange;
+
+    public void PlayerChange(int animalIndex)
+    {
+        if(event_PlayerChange != null)
+        {
+            event_PlayerChange(animalIndex);
+        }
+    }
+}
