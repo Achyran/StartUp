843e15dafedc93b952f25a92404b25823309f826
Amke Dijkstra <amkedijkstra@hotmail.com>
2021-02-15 15:21:02 +0100

    Created prefabs with correct model-texture combinations
    Not entirely sure if textures are correctly implemented. Might correct them later.

--- Assets/Scrips/ShowError.cs
+++ Assets/Scrips/ShowError.cs
@@ -5,24 +5,45 @@ using UnityEngine.UI;
 
 public class ShowError : MonoBehaviour
 {
+    //TODO: make public private?
     public Image errorImage;
     public Button errorButton;
+    public Button playButton;
+    public Button optionsButton;
+    public Button extrasButton;
+    public Button exitButton;
+    public Text errorText;
 
     private void Start()
     {
         errorImage.enabled = false;
         errorButton.gameObject.SetActive(false);
+        errorText.gameObject.SetActive(false);
     }
 
     public void OpenError()
     {
         errorImage.enabled = true;
         errorButton.gameObject.SetActive(true);
+        errorText.gameObject.SetActive(true);
+
+        //Disable main menu buttons
+        playButton.gameObject.SetActive(false);
+        optionsButton.gameObject.SetActive(false);
+        extrasButton.gameObject.SetActive(false);
+        exitButton.gameObject.SetActive(false);
     }
 
     public void CloseError()
     {
         errorImage.enabled = false;
         errorButton.gameObject.SetActive(false);
+        errorText.gameObject.SetActive(false);
+
+        //Enable main menu buttons
+        playButton.gameObject.SetActive(true);
+        optionsButton.gameObject.SetActive(true);
+        extrasButton.gameObject.SetActive(true);
+        exitButton.gameObject.SetActive(true);
     }
 }
