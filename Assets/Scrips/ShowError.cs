f31c551e4b32954dd3dfc832f82c535f0e84ce0b
Amke Dijkstra <amkedijkstra@hotmail.com>
2021-02-15 14:26:45 +0100

    Added an error-message for the Settings/Extras options
    Error-message needs artistic touch before release

---
+++ Assets/Scrips/ShowError.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowError : MonoBehaviour
+{
+    public Image errorImage;
+    public Button errorButton;
+
+    private void Start()
+    {
+        errorImage.enabled = false;
+        errorButton.gameObject.SetActive(false);
+    }
+
+    public void OpenError()
+    {
+        errorImage.enabled = true;
+        errorButton.gameObject.SetActive(true);
+    }
+
+    public void CloseError()
+    {
+        errorImage.enabled = false;
+        errorButton.gameObject.SetActive(false);
+    }
+}
