17d688c71d4bf803592785cc436229345a23a2c7
Amke Dijkstra <amkedijkstra@hotmail.com>
2021-02-15 12:04:56 +0100

    Created a main menu
    Did not change the build settings yet, so startGame() gives an error.

---
+++ Assets/Scrips/SceneManagement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("START GAME");
+        SceneManager.LoadScene("Tobi Sceene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT GAME");
+        Application.Quit();
+    }
+
+    public void ShowError()
+    {
+        Debug.Log("Sorry this function is not available yet");
+    }
+}
