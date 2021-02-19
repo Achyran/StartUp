9a01e7657f33c7b41d192e42d6c42748382578af
Premadness <65652200+Premadness@users.noreply.github.com>
2021-02-19 18:09:10 +0100

    Waterplants
    Added randomizer scripts
    Added water plants (might reimport for doublesided mesh

---
+++ Assets/Scrips/Randomizers/ChangeMaterial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    public List<Material> allMaterials;
+
+    private SkinnedMeshRenderer skinnedMeshRenderer;
+    private int randomMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomMaterial = Random.Range(0, allMaterials.Count);
+        skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        skinnedMeshRenderer.material = allMaterials[randomMaterial];
+    }
+}
