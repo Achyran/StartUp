13de21bd9faa875eb2fa101c2c2d6997476849fc
Premadness <65652200+Premadness@users.noreply.github.com>
2021-02-20 18:17:12 +0100

    Build push

---
+++ Assets/Scrips/DisableMouse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState =  CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MakeVisible();
+    }
+
+    private void MakeVisible() 
+    {
+        if (Input.GetKey(KeyCode.Tab) || Time.timeScale <= 0) 
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else 
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+}
